c0931bc8170e37b72e14b48e7a0e7cc28171e57d
pankona <yosuke.akatsuka@gmail.com>
2017-05-17 00:28:34 +0900

    animationSets implementation

--- simra/sprite.go
+++ simra/sprite.go
@@ -2,6 +2,7 @@ package simra
 
 import (
 	"image"
+	"time"
 
 	"github.com/pankona/gomo-simra/simra/peer"
 )
@@ -9,6 +10,34 @@ import (
 // Sprite represents a sprite object.
 type Sprite struct {
 	peer.Sprite
+	animationSets map[string]*AnimationSet
+}
+
+// AnimationSet represents a set of image for animation
+type AnimationSet struct {
+	textures []*Texture
+	interval time.Duration
+}
+
+// NewAnimationSet returns an instance of AnimationSet
+func NewAnimationSet() *AnimationSet {
+	LogDebug("IN")
+	LogDebug("OUT")
+	return &AnimationSet{}
+}
+
+// AddTexture adds a specified texture to AnimationSet
+func (animation *AnimationSet) AddTexture(texture *Texture) {
+	LogDebug("IN")
+	animation.textures = append(animation.textures, texture)
+	LogDebug("OUT")
+}
+
+// SetInterval sets interval of animation
+func (animation *AnimationSet) SetInterval(interval time.Duration) {
+	LogDebug("IN")
+	animation.interval = interval
+	LogDebug("OUT")
 }
 
 // ReplaceTexture replaces sprite's texture with specified image resource.
@@ -32,4 +61,20 @@ func (sprite *Sprite) RemoveAllTouchListener() {
 	LogDebug("IN")
 	sprite.Sprite.RemoveAllTouchListener()
 	LogDebug("OUT")
+}
+
+// AddAnimationSet adds a specified AnimationSet to sprite
+func (sprite *Sprite) AddAnimationSet(animationName string, set *AnimationSet) {
+	sprite.animationSets[animationName] = set
+}
+
+// StartAnimation starts animation by specified animation name
+func (sprite *Sprite) StartAnimation(animationName string) error {
+	// TODO: implement
+	return nil
+}
+
+// StopAnimation stops animation
+func (sprite *Sprite) StopAnimation() {
+	// TODO: implement
 }
