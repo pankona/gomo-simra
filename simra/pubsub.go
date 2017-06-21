6dbbd2a7b20c3d715b105164ab1daab20509fbf2
pankona <yosuke.akatsuka@gmail.com>
2017-06-21 10:16:35 +0900

    add comments on exported function and structs

--- simra/pubsub.go
+++ simra/pubsub.go
@@ -2,27 +2,32 @@ package simra
 
 import "sync"
 
+// Publisher is publisher interface for pubsub system
 type Publisher interface {
 	Publish(i interface{})
 	Subscribe(id string, s Subscriber) error
 	Unsubscribe(id string)
 }
 
+// Subscriber is subscriber interface for pubsub system
 type Subscriber interface {
 	OnEvent(i interface{})
 }
 
+// PubSub is an object to control publish and subscribe
 type PubSub struct {
 	m           sync.Mutex
 	subscribers map[string]Subscriber
 }
 
+// NewPubSub returns new PubSub instance
 func NewPubSub() *PubSub {
 	return &PubSub{
 		subscribers: make(map[string]Subscriber, 0),
 	}
 }
 
+// Publish publishes specified object to all subscriber
 func (p *PubSub) Publish(i interface{}) {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -32,6 +37,7 @@ func (p *PubSub) Publish(i interface{}) {
 	}
 }
 
+// Subscribe adds a subscriber to publisher
 func (p *PubSub) Subscribe(id string, s Subscriber) error {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -40,6 +46,7 @@ func (p *PubSub) Subscribe(id string, s Subscriber) error {
 	return nil
 }
 
+// Unsubscribe remove a subscriber
 func (p *PubSub) Unsubscribe(id string) {
 	delete(p.subscribers, id)
 }
--- simra/pubsub_test.go
+++ simra/pubsub_test.go
@@ -9,7 +9,7 @@ type sub struct {
 	f func()
 }
 
-func (s *sub) OnEvent(c Commander) {
+func (s *sub) OnEvent(i interface{}) {
 	s.f()
 }
 
@@ -23,16 +23,21 @@ func TestPubSub(t *testing.T) {
 		},
 	}
 
+	var err error
 	// discards duplicated registerations
-	p.Subscribe("subscriber1", s)
-	p.Subscribe("subscriber1", s)
+	err = p.Subscribe("subscriber1", s)
+	err = p.Subscribe("subscriber1", s)
 	wg.Add(1)
-	p.Subscribe("subscriber2", s)
-	p.Subscribe("subscriber2", s)
+	err = p.Subscribe("subscriber2", s)
+	err = p.Subscribe("subscriber2", s)
 	wg.Add(1)
-	p.Subscribe("subscriber3", s)
-	p.Subscribe("subscriber3", s)
+	err = p.Subscribe("subscriber3", s)
+	err = p.Subscribe("subscriber3", s)
 	wg.Add(1)
+
+	if err != nil {
+		t.Errorf("unexpected error. err = %s", err)
+	}
 
 	p.Publish(nil)
 	wg.Wait()
