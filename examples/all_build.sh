504e844244fe34525342b0620b9c09f5fb6965b7
pankona <yosuke.akatsuka@gmail.com>
2017-07-30 11:25:30 +0900

    update script to check build

--- examples/all_build.sh
+++ examples/all_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd ./sample1  && go build && cd -
-cd ./sample2  && go build && cd -
-cd ./sample3  && go build && cd -
-cd ./immortal && go build && cd -
+cd ./sample1    && go build && cd -
+cd ./sample2    && go build && cd -
+cd ./sample3    && go build && cd -
+cd ./immortal   && go build && cd -
+cd ./animation1 && go build && cd -
+cd ./animation2 && go build && cd -
+cd ./audio      && go build && cd -
+
