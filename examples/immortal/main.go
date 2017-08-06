0ef19801523d4c7f4bdcbe3c8009bcd3914e9913
pankona <yosuke.akatsuka@gmail.com>
2017-08-06 22:51:20 +0900

    update samples to follow changed interface

--- examples/immortal/main.go
+++ examples/immortal/main.go
@@ -3,33 +3,27 @@
 package main
 
 import (
-	"github.com/pankona/gomo-simra/examples/sample3/scene"
+	"github.com/pankona/gomo-simra/examples/immortal/scene"
 	"github.com/pankona/gomo-simra/simra"
 )
 
-func eventHandle(onStart, onStop chan bool) {
-	for {
-		select {
-		case <-onStart:
-			simra.LogDebug("receive chan. onStart")
-			engine := simra.GetInstance()
-			// TODO: this will be called on rotation.
-			// to keep state on rotation, SetScene must not call
-			// every onStart.
-			engine.SetScene(&scene.Title{})
-		case <-onStop:
-			simra.LogDebug("receive chan. onStop")
-		}
-	}
+func onStart() {
+	simra.LogDebug("receive chan. onStart")
+	engine := simra.GetInstance()
+	// TODO: this will be called on rotation.
+	// to keep state on rotation, SetScene must not call
+	// every onStart.
+	engine.SetScene(&scene.Title{})
+
+}
+
+func onStop() {
+	simra.LogDebug("receive chan. onStop")
 }
 
 func main() {
 	simra.LogDebug("[IN]")
 	engine := simra.GetInstance()
-
-	onStart := make(chan bool)
-	onStop := make(chan bool)
-	go eventHandle(onStart, onStop)
 	engine.Start(onStart, onStop)
 	simra.LogDebug("[OUT]")
 }
