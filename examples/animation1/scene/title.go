b4a56a1d723bf2e924d5de837ca025d81e893803
pankona <yosuke.akatsuka@gmail.com>
2017-09-25 19:54:05 +0900

    use short receiver name for animation1

--- examples/animation1/scene/title.go
+++ examples/animation1/scene/title.go
@@ -25,65 +25,65 @@ type Title struct {
 // This is called from simra.
 // simra.SetDesiredScreenSize should be called to determine
 // screen size of this scene.
-func (title *Title) Initialize(sim simra.Simraer) {
-	title.simra = sim
-	title.simra.SetDesiredScreenSize(ScreenWidth, ScreenHeight)
-	title.initialize()
+func (t *Title) Initialize(sim simra.Simraer) {
+	t.simra = sim
+	t.simra.SetDesiredScreenSize(ScreenWidth, ScreenHeight)
+	t.initialize()
 }
 
-func (title *Title) initialize() {
-	sprite := title.simra.NewSprite()
+func (t *Title) initialize() {
+	sprite := t.simra.NewSprite()
 	sprite.SetScale(ScreenWidth, 80)
 	sprite.SetPosition(ScreenWidth/2, ScreenHeight/2)
 
 	animationSet := simra.NewAnimationSet()
-	animationSet.AddTexture(title.simra.NewTextTexture("a", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("n", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("i", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("m", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("a", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("i", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("o", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("n", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("e", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("s", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
-	animationSet.AddTexture(title.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("a", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("n", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("i", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("m", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("a", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("i", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("o", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("n", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("e", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("s", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
+	animationSet.AddTexture(t.simra.NewTextTexture("t", 60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H)))
 
 	animationSet.SetInterval(12) // frames
 
 	sprite.AddAnimationSet("animation test", animationSet)
-	title.simra.AddSprite(sprite)
-	tex := title.simra.NewTextTexture("animation test",
+	t.simra.AddSprite(sprite)
+	tex := t.simra.NewTextTexture("animation test",
 		60, color.RGBA{255, 0, 0, 255}, image.Rect(0, 0, sprite.GetScale().W, sprite.GetScale().H))
 	sprite.ReplaceTexture(tex)
 
-	title.simra.AddTouchListener(title)
-	title.text = sprite
+	t.simra.AddTouchListener(t)
+	t.text = sprite
 }
 
 // Drive is called from simra.
 // This is used to update sprites position.
 // Thsi will be called 60 times per sec.
-func (title *Title) Drive() {
+func (t *Title) Drive() {
 }
 
 // OnTouchBegin is called when Title scene is Touched.
-func (title *Title) OnTouchBegin(x, y float32) {
+func (t *Title) OnTouchBegin(x, y float32) {
 }
 
 // OnTouchMove is called when Title scene is Touched and moved.
-func (title *Title) OnTouchMove(x, y float32) {
+func (t *Title) OnTouchMove(x, y float32) {
 }
 
 // OnTouchEnd is called when Title scene is Touched and it is released.
-func (title *Title) OnTouchEnd(x, y float32) {
-	if title.isAnimating {
-		title.text.StopAnimation()
-		title.isAnimating = false
+func (t *Title) OnTouchEnd(x, y float32) {
+	if t.isAnimating {
+		t.text.StopAnimation()
+		t.isAnimating = false
 	} else {
-		title.text.StartAnimation("animation test", true, func() {})
-		title.isAnimating = true
+		t.text.StartAnimation("animation test", true, func() {})
+		t.isAnimating = true
 	}
 }
