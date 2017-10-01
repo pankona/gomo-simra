6da498286b7679d8964647928b0e15d51b8c30ce
pankona <yosuke.akatsuka@gmail.com>
2017-10-01 14:11:46 +0900

    add missing comments for exposed functions

--- simra/image/image.go
+++ simra/image/image.go
@@ -2,10 +2,12 @@ package image
 
 import "image"
 
+// Rectangle is a wrapper of image.Rectangle
 type Rectangle struct {
 	image.Rectangle
 }
 
+// Rect returns a Rectangle instance
 func Rect(x0, y0, x1, y1 float32) Rectangle {
 	return Rectangle{
 		image.Rect(int(x0), int(y0), int(x1), int(y1)),
